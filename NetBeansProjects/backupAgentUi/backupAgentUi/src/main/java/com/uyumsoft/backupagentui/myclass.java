e72978e7ef5b4d8f7b877a76a783a583d91f569a
zeynep.oztemur <zeynep.oztemur@uyumsoft.com>
2019-06-21 15:16:57 +0300

    Duzenleme-1.0

---
+++ NetBeansProjects/backupAgentUi/backupAgentUi/src/main/java/com/uyumsoft/backupagentui/myclass.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uyumsoft.backupagentui;
+
+import java.io.File;
+
+/**
+ *
+ * @author zeynepoztemur
+ */
+public class myclass {
+    
+   public static void listFilesForFolder(final File folder) {
+    for (final File fileEntry : folder.listFiles()) {
+        if (fileEntry.isDirectory()) {
+            listFilesForFolder(fileEntry);
+        } else {
+            System.out.println(fileEntry.getName());
+        }
+    }
+}
+public static void main(String args[]){
+    
+String s1="my name is khan my name is java";
+String replaceString=s1.replace("is","was");
+
+System.out.println(replaceString);
+
+}
+}
