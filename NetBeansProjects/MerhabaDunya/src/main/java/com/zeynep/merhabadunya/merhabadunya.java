ff6255ecee05f1577c06a412628af322739f224c
zeynepoztemur.97 <zeynepoztemur.97@gmail.com>
2019-07-02 13:15:46 +0300

    ilk commit

---
+++ NetBeansProjects/MerhabaDunya/src/main/java/com/zeynep/merhabadunya/merhabadunya.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zeynep.merhabadunya;
+
+/**
+ *
+ * @author zeynepoztemur
+ */
+public class merhabadunya {
+    
+    public static void main(String args[]){
+    
+    System.out.println("Merhaba Dünya");
+    System.out.println("Videoyu beğendiyseniz beğen butonuna tıklamayı unutmayın :)");
+    System.out.println("Ve kanalıma abone olmak için abone ol butonuna tıklayabilirsiniz. ! ");
+    
+    
+    }
+    
+}
